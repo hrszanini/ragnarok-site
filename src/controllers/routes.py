fe5cc96882c7527ea86ec61f84e2e866256b34c6
Hamilton Zanini <hrszanini@gmail.com>
2021-02-20 02:56:51 -0300

    Correção de favicon

--- src/controllers/routes.py
+++ src/controllers/routes.py
@@ -7,6 +7,11 @@ def configure_page_routes(app: fastapi.FastAPI):
     def home_page():
         return html_file('index.html')
 
+    @app.get('/favicon.ico')
+    def favicon():
+        fav_file_path = '{}/favicon.ico'.format(services.RESOURCES_PATH)
+        return fastapi.responses.FileResponse(fav_file_path, media_type='img/ico')
+
     @app.get('/{file}')
     def html_file(file: str):
         html_file_path = '{}/html/{}'.format(services.RESOURCES_PATH, file)
@@ -29,7 +34,7 @@ def configure_page_routes(app: fastapi.FastAPI):
 
 
 def configure_api_routes(app: fastapi.FastAPI):
-    @app.get('/api/check_user/{user_id}')
+    @app.get('/api/check_user')
     def get_check_user(user_id: str):
         return services.check_user(user_id)
 
