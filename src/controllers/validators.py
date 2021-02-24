cd9dd0adf0c1d503373e954aa8de648a969334a7
Hamilton Zanini <hrszanini@gmail.com>
2021-02-23 22:19:20 -0300

    Inserção de MD5

--- src/controllers/validators.py
+++ src/controllers/validators.py
@@ -1,7 +1,10 @@
+from hashlib import md5
+
+
 def validate_password(password: str):
     if len(password) <= 4 or ';' in password:
         raise ValueError
-    return password
+    return md5(password.encode()).hexdigest()
 
 
 __all__ = ["validate_password"]
